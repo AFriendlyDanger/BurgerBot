d450e2380cd516d6d6bc5f6e4224446b4bc88b07
AFriendlyDanger <60414692+AFriendlyDanger@users.noreply.github.com>
2021-06-27 19:03:14 -0700

    Posts hamburger successfully

--- bot.js
+++ bot.js
@@ -1,12 +1,13 @@
-console.log('ayoo');
+console.log('Starting Bot...');
 
 require('dotenv').config();
 
 let burgerRE = /(\s|^)!burger(\s|$)/i
 let gelAuth = `&api_key=${process.env.GEL_API}&user_id=${process.env.GEL_USER}`
-let tags = 'burger+rating%3asafe'
+let gelImages = 'https://img3.gelbooru.com/images/'
+let tags = 'tags=burger+rating%3asafe+sort%3arandom'
 
-
+const fetch = require('node-fetch');
 const Discord = require('discord.js');
 const client = new Discord.Client();
 client.login(process.env.BOTTOKEN)
@@ -15,16 +16,31 @@ client.on('ready', botReady );
 
 function botReady(){
     console.log('Bot Ready');
+    client.user.setActivity("!burger", {type: "WATCHING"})
 }
 
 client.on('message', gotMessage);
 
-function gotMessage(msg){
+async function gotMessage(msg){
     if(msg.author.bot){
         return;
     }
     if (burgerRE.test(msg.content)){
-        msg.channel.send("Under construction")
+        //msg.channel.send("Under construction")
+        let post = await getImage();
+        msg.channel.send(post)
         return;
     }
+}
+
+async function getImage(){
+    let url = `https://gelbooru.com/index.php?page=dapi&s=post&q=index&json=1&${tags}&limit=1&pid=1${gelAuth}`;
+    let response = await fetch(url);
+    if (!response.ok) {
+        return ('Unable to grill, come back later.')
+    }
+    let json = await response.json();
+    let dir = json[0].directory;
+    let img = json[0].image;
+    return (`${gelImages}${dir}/${img}`)
 }
