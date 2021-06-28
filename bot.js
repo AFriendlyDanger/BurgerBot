80370193f0aba9ccb86ceb7dbd608ae28f301c6f
AFriendlyDanger <60414692+AFriendlyDanger@users.noreply.github.com>
2021-06-27 17:20:00 -0700

    added node fetch

--- bot.js
+++ bot.js
@@ -3,6 +3,9 @@ console.log('ayoo');
 require('dotenv').config();
 
 let burgerRE = /(\s|^)!burger(\s|$)/i
+let gelAuth = `&api_key=${process.env.GEL_API}&user_id=${process.env.GEL_USER}`
+let tags = 'burger+rating%3asafe'
+
 
 const Discord = require('discord.js');
 const client = new Discord.Client();
