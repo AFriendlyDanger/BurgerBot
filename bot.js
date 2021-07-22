da4bc5e08948294af01950872a9623ab1f5f1777
AFriendlyDanger <60414692+AFriendlyDanger@users.noreply.github.com>
2021-07-22 15:08:45 -0700

    new burger commands

--- bot.js
+++ bot.js
@@ -2,10 +2,19 @@ console.log('Starting Bot...');
 
 require('dotenv').config();
 
-let burgerRE = /(\s|^)!burger(\s|$)/i
+let burgerRE = /(\s|^)!burg(e|o)r(\s|$)/i
+let mcdonRE = /(\s|^)!(m|w)cdonald('*s|s*)(\s|$)/i
+let bkRE = /(\s|^)!burger_*king(\s|$)/i
+let wendyRE = /(\s|^)!wendy'*s(\s|$)/i
+let burgerTag = 'burger'
+let mcTag = 'mcdonald\'s'
+let bkTag = 'burger_king'
+let wenTag = 'wendy\'s'
+
 let gelAuth = `&api_key=${process.env.GEL_API}&user_id=${process.env.GEL_USER}`
 let gelImages = 'https://img3.gelbooru.com/images/'
-let tags = 'tags=burger+rating%3asafe+sort%3arandom'
+//let tags = 'tags=burger+rating%3asafe+sort%3arandom'
+let safeSearchTag = 'rating%3asafe+sort%3arandom'
 
 const fetch = require('node-fetch');
 const Discord = require('discord.js');
@@ -25,19 +34,42 @@ async function gotMessage(msg){
     if(msg.author.bot){
         return;
     }
-    if (burgerRE.test(msg.content)){
-        //msg.channel.send("Under construction")
-        let post = await getImage();
+    var content = msg.content;
+    var tag = ''
+    switch(true)
+    {
+        case burgerRE.test(content):
+            tag = burgerTag;
+            break;
+        case mcdonRE.test(content):
+            tag = mcTag;
+            break;
+        case bkRE.test(content):
+            tag = bkTag;
+            break;
+        case wendyRE.test(content):
+            tag = wenTag;
+            break;
+    }
+    if (tag != ''){
+        let post = await getImage(tag);
         msg.channel.send(post)
         return;
     }
+    // if (burgerRE.test(msg.content)){
+    //     //msg.channel.send("Under construction")
+    //     let post = await getImage();
+    //     msg.channel.send(post)
+    //     return;
+    // }
 }
 
-async function getImage(){
+async function getImage(tag = 'burger'){
+    let tags = `tags=${tag}+${safeSearchTag}`;
     let url = `https://gelbooru.com/index.php?page=dapi&s=post&q=index&json=1&${tags}&limit=1&pid=1${gelAuth}`;
     let response = await fetch(url);
     if (!response.ok) {
-        return ('Unable to grill, come back later.')
+        return ('Unable to grill, come back later.');
     }
     let json = await response.json();
     let dir = json[0].directory;
